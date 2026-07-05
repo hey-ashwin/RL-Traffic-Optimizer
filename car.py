35b124470029ed5cbdda76d6788ca3e973be246b
Ashwin Singh <265865666+hey-ashwin@users.noreply.github.com>
2026-07-05 13:50:24 +0530

    Delete car.py

--- car.py
+++
@@ -1,5 +0,0 @@
-class Car:
-    def __init__(self, entry_time):
-        self.entry_time = entry_time
-
-# we are keeping a track of entry time of vehicles to use is calculation of total waiting time later
